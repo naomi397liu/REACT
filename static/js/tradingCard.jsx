8ff792a7cea8791418e8db6de427ec39db7aee57
Naomi Liu <nhliu@ucdavis.edu>
2020-10-23 23:34:14 +0000

    Added 2 trading cards

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -12,7 +12,7 @@ function TradingCard(props) {
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
@@ -48,4 +48,26 @@ ReactDOM.render(
     />
   ),
   document.querySelector('#llambda')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Seedpy"
+      skill="Cookie baking"
+      imgUrl="/static/img/seedpy.jpg"
+    />
+  ),
+  document.querySelector('#seedpy')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Polymorphism"
+      skill="Morphing"
+      imgUrl="/static/img/polymorphism.jpg"
+    />
+  ),
+  document.querySelector('#polymorphism')
 );
